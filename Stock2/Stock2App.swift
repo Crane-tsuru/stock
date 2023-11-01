2faf4bab1af18d7190b8ba84c2a5ed42a79407ed
Crane-tsuru <116482604+Crane-tsuru@users.noreply.github.com>
2023-11-01 18:11:58 +0900

    Initial Commit

---
+++ Stock2/Stock2App.swift
@@ -0,0 +1,17 @@
+//
+//  Stock2App.swift
+//  Stock2
+//
+//  Created by 鶴見駿 on 2023/11/01.
+//
+
+import SwiftUI
+
+@main
+struct Stock2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
